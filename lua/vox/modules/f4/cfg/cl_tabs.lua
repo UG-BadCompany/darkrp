c4a87740619f55935fed67ced3310d63dda162cf
UG-BadCompany <63736603+UG-BadCompany@users.noreply.github.com>
2026-06-22 16:30:57 -0700

    Redesign F4 premium dashboard tabs

--- lua/vox/modules/f4/cfg/cl_tabs.lua
+++ lua/vox/modules/f4/cfg/cl_tabs.lua
@@ -23,3 +23,28 @@ vox.f4:RegisterTab('shop', {
     icon = 'https://i.imgur.com/duyBVAS.png',
     class = 'vox.f4.Shop'
 })
+
+
+vox.f4:RegisterTab('inventory', {
+    order = 4,
+    name = 'INVENTORY',
+    desc = 'Storage, identity, and roleplay items',
+    icon = 'https://i.imgur.com/ECLKU9s.png',
+    class = 'vox.f4.Inventory'
+})
+
+vox.f4:RegisterTab('upgrades', {
+    order = 5,
+    name = 'UPGRADES',
+    desc = 'Progression and premium perks',
+    icon = 'https://i.imgur.com/l4M12dO.png',
+    class = 'vox.f4.Upgrades'
+})
+
+vox.f4:RegisterTab('settings', {
+    order = 6,
+    name = 'SETTINGS',
+    desc = 'Theme, scale, previews, accessibility',
+    icon = 'https://i.imgur.com/41kCW0x.png',
+    class = 'vox.f4.Settings'
+})
