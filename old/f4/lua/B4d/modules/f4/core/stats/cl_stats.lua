1e47ebc4d20167229fd31715da516eec189b587b
UG-BadCompany <63736603+UG-BadCompany@users.noreply.github.com>
2026-06-22 04:59:44 -0700

    update

---
+++ old/f4/lua/B4d/modules/f4/core/stats/cl_stats.lua
@@ -0,0 +1,24 @@
+--leak by matveicher
+--vk group - https://vk.com/gmodffdev
+--steam - https://steamcommunity.com/profiles/76561198968457747/
+--ds server - https://discord.gg/V329W7Ce8g
+--ds - matveicher
+
+--[[
+
+Author: tochnonement
+Email: tochnonement@gmail.com
+
+03/01/2024
+
+--]]
+
+netchunk.Callback('onyx.f4:SendStats', function(data)
+    hook.Run('onyx.f4.StatsReceived', data)
+end)
+
+--leak by matveicher
+--vk group - https://vk.com/gmodffdev
+--steam - https://steamcommunity.com/profiles/76561198968457747/
+--ds server - https://discord.gg/V329W7Ce8g
+--ds - matveicher
